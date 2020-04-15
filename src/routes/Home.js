7da6aaec32d48359e618001419821819e3f5286d
Dasol Park <dasol.park615@gmail.com>
2020-04-16 00:46:42 +0900

    Home done(header, section-grid, footer)

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,210 +3,224 @@ import { Link } from 'react-router-dom';
 import './Home.css';
 
 import FormValidator from '../images/form-validator.JPG';
+import ReactLogo from '../images/logo192.png';
 
 const Home = () => {
-  return <div className="home__wrapper">
-    <div>
-      <img src={FormValidator} alt="Form-validator"></img>
-      <div className="content">
-        <h4>Form Validation</h4>
-        <p>Using DOM & Events</p>
-        <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
-          Live Demo
-        </Link>
-      </div>
+  return (
+    <div className="home__wrapper">
+      <header className="showcase bg-primary">
+        <img src={ReactLogo} alt="react-logo"></img>
+        <h1>20 Web Projects With React.js</h1>
+      </header>
+      <section className="projects">
+        <div>
+          <img src={FormValidator} alt="Form-validator"></img>
+          <div className="content">
+            <h4>Form Validation</h4>
+            <p>Using DOM & Events</p>
+            <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
+              Go to project
+            </Link>
+          </div>
+        </div>
+        <div>
+          <img src={FormValidator} alt="Form-validator"></img>
+          <div className="content">
+            <h4>Form Validation</h4>
+            <p>Using DOM & Events</p>
+            <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
+              Go to project
+            </Link>
+          </div>
+        </div>
+        <div>
+          <img src={FormValidator} alt="Form-validator"></img>
+          <div className="content">
+            <h4>Form Validation</h4>
+            <p>Using DOM & Events</p>
+            <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
+              Go to project
+            </Link>
+          </div>
+        </div>
+        <div>
+          <img src={FormValidator} alt="Form-validator"></img>
+          <div className="content">
+            <h4>Form Validation</h4>
+            <p>Using DOM & Events</p>
+            <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
+              Go to project
+            </Link>
+          </div>
+        </div>
+        <div>
+          <img src={FormValidator} alt="Form-validator"></img>
+          <div className="content">
+            <h4>Form Validation</h4>
+            <p>Using DOM & Events</p>
+            <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
+              Go to project
+            </Link>
+          </div>
+        </div>
+        <div>
+          <img src={FormValidator} alt="Form-validator"></img>
+          <div className="content">
+            <h4>Form Validation</h4>
+            <p>Using DOM & Events</p>
+            <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
+              Go to project
+            </Link>
+          </div>
+        </div>
+        <div>
+          <img src={FormValidator} alt="Form-validator"></img>
+          <div className="content">
+            <h4>Form Validation</h4>
+            <p>Using DOM & Events</p>
+            <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
+              Go to project
+            </Link>
+          </div>
+        </div>
+        <div>
+          <img src={FormValidator} alt="Form-validator"></img>
+          <div className="content">
+            <h4>Form Validation</h4>
+            <p>Using DOM & Events</p>
+            <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
+              Go to project
+            </Link>
+          </div>
+        </div>
+        <div>
+          <img src={FormValidator} alt="Form-validator"></img>
+          <div className="content">
+            <h4>Form Validation</h4>
+            <p>Using DOM & Events</p>
+            <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
+              Go to project
+            </Link>
+          </div>
+        </div>
+        <div>
+          <img src={FormValidator} alt="Form-validator"></img>
+          <div className="content">
+            <h4>Form Validation</h4>
+            <p>Using DOM & Events</p>
+            <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
+              Go to project
+            </Link>
+          </div>
+        </div>
+        <div>
+          <img src={FormValidator} alt="Form-validator"></img>
+          <div className="content">
+            <h4>Form Validation</h4>
+            <p>Using DOM & Events</p>
+            <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
+              Go to project
+            </Link>
+          </div>
+        </div>
+        <div>
+          <img src={FormValidator} alt="Form-validator"></img>
+          <div className="content">
+            <h4>Form Validation</h4>
+            <p>Using DOM & Events</p>
+            <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
+              Go to project
+            </Link>
+          </div>
+        </div>
+        <div>
+          <img src={FormValidator} alt="Form-validator"></img>
+          <div className="content">
+            <h4>Form Validation</h4>
+            <p>Using DOM & Events</p>
+            <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
+              Go to project
+            </Link>
+          </div>
+        </div>
+        <div>
+          <img src={FormValidator} alt="Form-validator"></img>
+          <div className="content">
+            <h4>Form Validation</h4>
+            <p>Using DOM & Events</p>
+            <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
+              Go to project
+            </Link>
+          </div>
+        </div>
+        <div>
+          <img src={FormValidator} alt="Form-validator"></img>
+          <div className="content">
+            <h4>Form Validation</h4>
+            <p>Using DOM & Events</p>
+            <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
+              Go to project
+            </Link>
+          </div>
+        </div>
+        <div>
+          <img src={FormValidator} alt="Form-validator"></img>
+          <div className="content">
+            <h4>Form Validation</h4>
+            <p>Using DOM & Events</p>
+            <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
+              Go to project
+            </Link>
+          </div>
+        </div>
+        <div>
+          <img src={FormValidator} alt="Form-validator"></img>
+          <div className="content">
+            <h4>Form Validation</h4>
+            <p>Using DOM & Events</p>
+            <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
+              Go to project
+            </Link>
+          </div>
+        </div>
+        <div>
+          <img src={FormValidator} alt="Form-validator"></img>
+          <div className="content">
+            <h4>Form Validation</h4>
+            <p>Using DOM & Events</p>
+            <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
+              Go to project
+            </Link>
+          </div>
+        </div>
+        <div>
+          <img src={FormValidator} alt="Form-validator"></img>
+          <div className="content">
+            <h4>Form Validation</h4>
+            <p>Using DOM & Events</p>
+            <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
+              Go to project
+            </Link>
+          </div>
+        </div>
+        <div>
+          <img src={FormValidator} alt="Form-validator"></img>
+          <div className="content">
+            <h4>Form Validation</h4>
+            <p>Using DOM & Events</p>
+            <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
+              Go to project
+            </Link>
+          </div>
+        </div>
+      </section>
+      <footer className="main-footer bg-primary">
+        <a href="https://github.com/DasolPark/Dasol_React_20_Web_Projects" target="_blank" rel="noopener noreferrer">
+          <h2>> Projects Github Repo</h2>
+        </a>
+      </footer>
     </div>
-    <div>
-      <img src={FormValidator} alt="Form-validator"></img>
-      <div className="content">
-        <h4>Form Validation</h4>
-        <p>Using DOM & Events</p>
-        <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
-          Live Demo
-        </Link>
-      </div>
-    </div>
-    <div>
-      <img src={FormValidator} alt="Form-validator"></img>
-      <div className="content">
-        <h4>Form Validation</h4>
-        <p>Using DOM & Events</p>
-        <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
-          Live Demo
-        </Link>
-      </div>
-    </div>
-    <div>
-      <img src={FormValidator} alt="Form-validator"></img>
-      <div className="content">
-        <h4>Form Validation</h4>
-        <p>Using DOM & Events</p>
-        <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
-          Live Demo
-        </Link>
-      </div>
-    </div>
-    <div>
-      <img src={FormValidator} alt="Form-validator"></img>
-      <div className="content">
-        <h4>Form Validation</h4>
-        <p>Using DOM & Events</p>
-        <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
-          Live Demo
-        </Link>
-      </div>
-    </div>
-    <div>
-      <img src={FormValidator} alt="Form-validator"></img>
-      <div className="content">
-        <h4>Form Validation</h4>
-        <p>Using DOM & Events</p>
-        <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
-          Live Demo
-        </Link>
-      </div>
-    </div>
-    <div>
-      <img src={FormValidator} alt="Form-validator"></img>
-      <div className="content">
-        <h4>Form Validation</h4>
-        <p>Using DOM & Events</p>
-        <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
-          Live Demo
-        </Link>
-      </div>
-    </div>
-    <div>
-      <img src={FormValidator} alt="Form-validator"></img>
-      <div className="content">
-        <h4>Form Validation</h4>
-        <p>Using DOM & Events</p>
-        <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
-          Live Demo
-        </Link>
-      </div>
-    </div>
-    <div>
-      <img src={FormValidator} alt="Form-validator"></img>
-      <div className="content">
-        <h4>Form Validation</h4>
-        <p>Using DOM & Events</p>
-        <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
-          Live Demo
-        </Link>
-      </div>
-    </div>
-    <div>
-      <img src={FormValidator} alt="Form-validator"></img>
-      <div className="content">
-        <h4>Form Validation</h4>
-        <p>Using DOM & Events</p>
-        <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
-          Live Demo
-        </Link>
-      </div>
-    </div>
-    <div>
-      <img src={FormValidator} alt="Form-validator"></img>
-      <div className="content">
-        <h4>Form Validation</h4>
-        <p>Using DOM & Events</p>
-        <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
-          Live Demo
-        </Link>
-      </div>
-    </div>
-    <div>
-      <img src={FormValidator} alt="Form-validator"></img>
-      <div className="content">
-        <h4>Form Validation</h4>
-        <p>Using DOM & Events</p>
-        <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
-          Live Demo
-        </Link>
-      </div>
-    </div>
-    <div>
-      <img src={FormValidator} alt="Form-validator"></img>
-      <div className="content">
-        <h4>Form Validation</h4>
-        <p>Using DOM & Events</p>
-        <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
-          Live Demo
-        </Link>
-      </div>
-    </div>
-    <div>
-      <img src={FormValidator} alt="Form-validator"></img>
-      <div className="content">
-        <h4>Form Validation</h4>
-        <p>Using DOM & Events</p>
-        <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
-          Live Demo
-        </Link>
-      </div>
-    </div>
-    <div>
-      <img src={FormValidator} alt="Form-validator"></img>
-      <div className="content">
-        <h4>Form Validation</h4>
-        <p>Using DOM & Events</p>
-        <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
-          Live Demo
-        </Link>
-      </div>
-    </div>
-    <div>
-      <img src={FormValidator} alt="Form-validator"></img>
-      <div className="content">
-        <h4>Form Validation</h4>
-        <p>Using DOM & Events</p>
-        <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
-          Live Demo
-        </Link>
-      </div>
-    </div>
-    <div>
-      <img src={FormValidator} alt="Form-validator"></img>
-      <div className="content">
-        <h4>Form Validation</h4>
-        <p>Using DOM & Events</p>
-        <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
-          Live Demo
-        </Link>
-      </div>
-    </div>
-    <div>
-      <img src={FormValidator} alt="Form-validator"></img>
-      <div className="content">
-        <h4>Form Validation</h4>
-        <p>Using DOM & Events</p>
-        <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
-          Live Demo
-        </Link>
-      </div>
-    </div>
-    <div>
-      <img src={FormValidator} alt="Form-validator"></img>
-      <div className="content">
-        <h4>Form Validation</h4>
-        <p>Using DOM & Events</p>
-        <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
-          Live Demo
-        </Link>
-      </div>
-    </div>
-    <div>
-      <img src={FormValidator} alt="Form-validator"></img>
-      <div className="content">
-        <h4>Form Validation</h4>
-        <p>Using DOM & Events</p>
-        <Link className="btn btn-primary" to={{ pathname: '/form-validator' }}>
-          Live Demo
-        </Link>
-      </div>
-    </div>
-  </div>
+  )
 }
 
 export default Home;
