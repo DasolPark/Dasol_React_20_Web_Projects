da8e1388d758c6ef61ca400bfd70a893d14f2b65
Dasol Park <dasol.park615@gmail.com>
2020-04-17 01:29:33 +0900

    [Movie Seat Booking] Functionality(v0)

--- src/components/MovieSeatBooking.js
+++ src/components/MovieSeatBooking.js
@@ -3,17 +3,93 @@ import React from 'react';
 import './MovieSeatBooking.css';
 
 export default class MovieSeatBooking extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ticketPrice: 12000 }
+
+    this.seats = React.createRef();
+    this.count = React.createRef();
+    this.total = React.createRef();
+  }
+
+  componentDidMount() {
+    this.populateUI();
+  }
+
+  componentDidUpdate() {
+    this.updateSelectedCount();
+  }
+
+  // Populate UI
+  populateUI() {
+    const selectedIndex = JSON.parse(localStorage.getItem(`selectedIndex_r`));
+    console.log(selectedIndex);
+    console.log(this.seats.current.childNodes);
+
+    if (selectedIndex !== null && selectedIndex.length > 0) {
+      [...this.seats.current.childNodes].forEach(el => {
+        if (el.classList.value === 'row') {
+          [...el.childNodes].forEach((seat, index) => {
+            if (selectedIndex[index] && selectedIndex[index].indexOf[index] > -1) {
+              seat.classList.add('selected');
+            }
+          })
+        }
+      })
+    }
+  }
+
+  // Update selected count
+  updateSelectedCount() {
+    const selectedIndex = [];
+    const selectedSeats = [...this.seats.current.childNodes].map(item => {
+      const arr = [];
+      if (item.classList.value === 'row') {
+        [...item.childNodes].map(item => {
+          if (item.classList.contains('seat') && item.classList.contains('selected')) {
+            arr.push(item);
+          }
+          return arr;
+        });
+
+        selectedIndex.push(arr.map(seat => [...item.childNodes].indexOf(seat)));
+      }
+      return arr;
+    })
+
+    localStorage.setItem(`selectedIndex_r`, JSON.stringify(selectedIndex));
+
+    const selectedSeatsTotal = selectedSeats.reduce((acc, val) => acc + val.length, 0)
+
+    this.count.current.innerText = selectedSeatsTotal;
+    this.total.current.innerText = selectedSeatsTotal * this.state.ticketPrice;
+  }
+
+  // Change option event
+  onChangeSelect = (e) => {
+    this.setState({ ticketPrice: +e.target.value });
+  }
+
+  // Click seat event
+  onSeatClick = (e) => {
+    if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+      e.target.classList.toggle('selected');
+      this.updateSelectedCount();
+    }
+  }
+
   render() {
     return (
       <div className="movie__container">
 
         <div className="movie-selection">
           <label><span role="img" aria-labelledby="emoji">🎥</span>영화를 선택해주세요 </label>
-          <select id="movie">
-            <option value="12">기생충(12000₩)</option>
-            <option value="10">결혼 이야기(10000₩)</option>
-            <option value="12">1917(9000₩)</option>
-            <option value="12">조커(7000₩)</option>
+          <select value={this.state.ticketPrice} onChange={this.onChangeSelect} >
+            <option value="12000">기생충(Parasite) [₩12000]</option>
+            <option value="10000">결혼 이야기(Marrige Story) [₩10000]</option>
+            <option value="9000">1917 [₩9000]</option>
+            <option value="7000">조커(Joker) [₩7000]</option>
           </select>
         </div>
 
@@ -32,7 +108,7 @@ export default class MovieSeatBooking extends React.Component {
           </li>
         </ul>
 
-        <div className="seats">
+        <div ref={this.seats} className="seats" onClick={this.onSeatClick}>
           <div className="screen"></div>
 
           <div className="row">
@@ -98,8 +174,8 @@ export default class MovieSeatBooking extends React.Component {
         </div>
 
         <p className="text">
-          고객님이 선택한 좌석은 <span id="count">0</span>개 이며,
-          총 가격은 <span id="total">0</span>₩ 입니다.
+          고객님이 선택한 좌석은 <span ref={this.count}>0</span>개 이며,
+          총 가격은 ₩<span ref={this.total}>0</span> 입니다.
         </p>
       </div>
     )
